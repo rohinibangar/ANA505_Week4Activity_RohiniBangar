22cfe314a53f110c32bfdc1f297d336cd409876d
rohinibangar <149907589+rohinibangar@users.noreply.github.com>
2023-11-19 12:23:46 -0500

    Add files via upload

---
+++ ANA505_Week4_Activity.R
@@ -0,0 +1,51 @@
+#Task: Write the function to get a dataset from Base R: Titanic
+#and give the dataframe a new name of your choice
+
+data(Titanic)
+Passengers <- as.data.frame(Titanic)
+
+#TASK: Write the function to see the top rows of the data
+head(Passengers,4)
+
+#Install and call the package dplyr
+install.packages("dplyr")
+library("dplyr")
+
+#Task: Write the function to 'select' the Survived and Sex columns 
+select(Passengers,Survived,Sex)
+
+#TASK: Write the function to save the two columns as one new dataset
+#and give it a name
+PassengersNewData <- select(Passengers,Survived,Sex)
+
+#TASK: Write the function that deselects the sex column
+select(PassengersNewData,Survived,-Sex)
+
+#TASK: Write the function that renames 'Sex' to 'Gender'
+rename(PassengersNewData, gender=Sex)
+
+#TASK: Write the function that names a new dataset that includes the 'gender' column
+PassengersV1<-mutate(Passengers, gender=Sex)
+
+#TASK: Write the function that includes only rows that are 'male'
+males<-filter(PassengersV1, gender=="Male")
+
+#TASK: Write the function to group the data by gender (hint: arrange())
+arrange(males, Freq)
+group_by(males, gender)
+
+#TASK: Sum the Freq column
+#TASK: After you run it, write the total here:____
+sum(Passengers$Freq)
+total-2201
+
+#TASK: Write the function that includes only rows that are 'female'
+females<-filter(PassengersV1, gender=="Female")
+
+#TASK: Write the function that joins the male and female rows 
+union(males,females)
+
+
+#Optional Task: add any of the other functions 
+#you learned about from the dplyr package
+setdiff(males,females)  # The setdiff() function is used to get the difference between two sets of values. It returns the values in the first set that are not present in the second set.
